a28c59c24c56f259df932d6873aa71f0b9ac1ba4
AndyKallian <andersonkallian@yahoo.com.br>
2023-07-29 00:15:05 -0300

    fixing navbar

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 window.addEventListener('scroll', onScroll)
+const navigation = document.querySelector('#navigation')
+const backToTopButton = document.querySelector('#backToTopButton')
+
 
 onScroll()
 function onScroll() {
@@ -16,7 +19,6 @@ function activateMenuAtCurrentSection(section) {
   const targetLine = scrollY + innerHeight / 2
 
   // verificar se a seção passou da linha
-  // quais dados vou precisar?
   const sectionTop = section.offsetTop
   const sectionHeight = section.offsetHeight
   const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
@@ -38,6 +40,8 @@ function activateMenuAtCurrentSection(section) {
     menuElement.classList.add('active')
   }
 }
+
+
 
 function showNavOnScroll() {
   if (scrollY > 0) {
